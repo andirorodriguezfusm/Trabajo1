3f12489e3ec2970ba3605bc90f3e1bf71b5af809
andrew ditson rodriguez sanchez <ar131505@ingenieria.sanmartin.edu.co>
2014-02-23 19:20:01 -0500

    commit inicial del trabajo de colisiones

---
+++ Assets/scripts/OnCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCollider : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision){
+		Debug.Log("Collision Enter.");
+	}
+	void OnCollisionStay(Collision collision){
+		Debug.Log("Collision Stay.");
+	}
+	void OnCollisionExit(Collision collision){
+		Debug.Log("Collision Exit.");
+	}
+}
---
+++ Assets/scripts/OnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTrigger : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider collider){
+		Debug.Log("Collider Enter.");
+	}
+	void OnTriggerStay(Collider collider){
+		Debug.Log("Collider Stay.");
+	}
+	void OnTriggerExit(Collider collider){
+		Debug.Log("Collider Exit.");
+	}
+}
